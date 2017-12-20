8946c4f3e9f3d129e38cad487e344075825b1455
Kongan Cossec <konogan@gmail.com>
2017-12-20 16:19:02 +0100

    cleaning

--- scripts/rss.js
+++ scripts/rss.js
@@ -3,51 +3,61 @@
 const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser());
 
 fetch('urls.json')
-  .then((res) => {
-    res.text().then((data) => {
-      var frag = document.createDocumentFragment();
-      var hasBegun = true;
-      JSON.parse(data).urls.forEach((u) => {
-        try {
-          var url = new URL(u)
-        }
-        catch (e) {
-          console.error('URL invalid');
-          return
-        }
+  .then((jsonUrlContent) => {
+    jsonUrlContent
+      .text()
+      .then((jsonUrlContentText) => {
+        let frag = document.createDocumentFragment();
+        let debut = true;
+        JSON.parse(jsonUrlContentText).urls.forEach((u) => {
+          try {
+            var url = new URL(u)
+          }
+          catch (e) {
+            console.error('URL invalid');
+            return
+          }
 
-        fetch(url).then((res) => {
-          res.text().then((xmlTxt) => {
-            try {
-              let doc = DOMPARSER(xmlTxt, "text/xml");
-              let heading = document.createElement('h1');
-              heading.textContent = url.hostname;
-              frag.appendChild(heading);
-              doc.querySelectorAll('item').forEach((item) => {
-                let temp = document.importNode(document.querySelector('template').content, true);
-                let i = item.querySelector.bind(item);
-                let t = temp.querySelector.bind(temp);
-                t('h2').textContent = !!i('title') ? i('title').textContent : '-';
-                t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
-                t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
-                t('h3').textContent = url.hostname;
-                frag.appendChild(temp);
-              })
-            } catch (e) {
-              console.error('Error in parsing the feed');
-            }
-            if (hasBegun) {
-              document.querySelector('output').textContent = '';
-              hasBegun = false;
-            }
-            document.querySelector('output').appendChild(frag);
-          })
-        }).catch((err) => {
-          console.error('Error in fetching the RSS feed', err);
-        });
+          fetch(url)
+            .then((content) => {
+              content
+                .text()
+                .then((contentXML) => {
+                  try {
+                    let doc = DOMPARSER(contentXML, 'text/xml');
 
+                    let titre = document.createElement('h1');
+                    titre.textContent = url.hostname;
+                    frag.appendChild(titre);
+
+                    doc
+                      .querySelectorAll('item')
+                      .forEach((item) => {
+                        console.log(item);
+                        let temp = document.importNode(document.querySelector('template').content, true);
+                        let i = item.querySelector.bind(item);
+                        let t = temp.querySelector.bind(temp);
+                        t('h2').textContent = !!i('title') ? i('title').textContent : '-';
+                        t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
+                        t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
+                        t('h3').textContent = url.hostname;
+                        frag.appendChild(temp);
+                      })
+                  } catch (e) {
+                    console.error('Error in parsing the XML', e);
+                  }
+                  if (debut) {
+                    document.querySelector('output').textContent = '';
+                    debut = false;
+                  }
+                  document.querySelector('output').appendChild(frag);
+                })
+            }).catch((err) => {
+              console.error('Error in fetching the RSS feed', err);
+            });
+
+        })
       })
-    })
   }).catch((err) => {
     console.error('Error in fetching the URLs json', err);
   });
